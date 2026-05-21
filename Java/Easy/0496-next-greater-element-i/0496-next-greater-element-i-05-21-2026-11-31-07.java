c232019dc5a62e7c5fd96b04a4022b64c8fdd58e
Mithil Gala <mithil.gala@gmail.com>
2026-05-21 11:31:10 +0530

    4 ms (75.6%) 45.1 MB (69.5%) 05-21-2026 0496-next-greater-element-i Easy .java Array

---
+++ Java/Easy/0496-next-greater-element-i/0496-next-greater-element-i-05-21-2026-11-31-07.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> st = new Stack<>();
+
+        for (int num : nums2) {
+            while (!st.isEmpty() && num > st.peek()) {
+                map.put(st.pop(), num);
+            }
+            st.push(num);
+        }
+
+        while (!st.isEmpty()) map.put(st.pop(), -1);
+
+        for (int i = 0; i < nums1.length; i++) {
+            nums1[i] = map.get(nums1[i]);
+        }
+        return nums1;
+    }
+}
