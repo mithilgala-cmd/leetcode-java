2b7cd41f4d095fcd7968a3d098716094e2eb11da
Mithil Gala <mithil.gala@gmail.com>
2026-05-26 09:17:36 +0530

    2 ms (98.07%) 45 MB (57.15%) 05-26-2026 0349-intersection-of-two-arrays Easy .java Array

---
+++ Java/Easy/0349-intersection-of-two-arrays/0349-intersection-of-two-arrays-05-26-2026-09-17-34.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        java.util.HashSet<Integer> set1 = new java.util.HashSet<>();
+        java.util.HashSet<Integer> result = new java.util.HashSet<>();
+        
+        for (int num : nums1) {
+            set1.add(num);
+        }
+        
+        for (int num : nums2) {
+            if (set1.contains(num)) {
+                result.add(num);
+            }
+        }
+        
+        int[] res = new int[result.size()];
+        int i = 0;
+        for (int num : result) {
+            res[i++] = num;
+        }        
+        return res;
+    }
+}
