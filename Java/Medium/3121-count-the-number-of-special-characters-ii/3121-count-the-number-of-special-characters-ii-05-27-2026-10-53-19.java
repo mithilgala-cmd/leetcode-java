06528ca058d053e87333436b5117f7edb60db823
Mithil Gala <mithil.gala@gmail.com>
2026-05-27 10:53:17 +0530

    21 ms (66.94%) 48.3 MB (34.3%) 05-27-2026 3121-count-the-number-of-special-characters-ii Medium .java Hash Table

---
+++ Java/Medium/3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii-05-27-2026-10-53-19.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int numberOfSpecialChars(String word) {
+        int[] lower = new int[26];
+        int[] upper = new int[26];
+        
+        java.util.Arrays.fill(lower, -1);
+        java.util.Arrays.fill(upper, -1);
+        
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            
+            if (Character.isLowerCase(ch)) {
+                lower[ch - 'a'] = i; 
+            } else {
+                if (upper[ch - 'A'] == -1) {
+                    upper[ch - 'A'] = i;
+                }
+            }
+        }
+        
+        int count = 0;
+        
+        for (int i = 0; i < 26; i++) {
+            if (lower[i] != -1 && upper[i] != -1 && lower[i] < upper[i]) {
+                count++;
+            }
+        }        
+        return count;
+    }
+}
