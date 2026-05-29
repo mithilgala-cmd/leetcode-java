d11254449cd321ae54deab976be304137422b830
Mithil Gala <mithil.gala@gmail.com>
2026-05-29 12:48:18 +0530

    3 ms (70.11%) 53.5 MB (66.47%) 05-29-2026 0997-find-the-town-judge Easy .java Array

---
+++ Java/Easy/0997-find-the-town-judge/0997-find-the-town-judge-05-29-2026-12-48-20.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        if (n == 1 && trust.length == 0) {
+            return 1;
+        }
+
+        int[] trustScores = new int[n + 1];
+
+        for (int[] relation : trust) {
+            int a = relation[0];
+            int b = relation[1];
+            
+            trustScores[a]--;
+            trustScores[b]++;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (trustScores[i] == n - 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
