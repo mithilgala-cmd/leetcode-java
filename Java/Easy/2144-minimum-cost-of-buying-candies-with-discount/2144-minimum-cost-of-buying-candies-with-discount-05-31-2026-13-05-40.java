48ec7579fead268f5712b9ebd3e33340332152eb
Mithil Gala <mithil.gala@gmail.com>
2026-05-31 13:05:43 +0530

    6 ms (41.4%) 44.5 MB (50.85%) 05-31-2026 2144-minimum-cost-of-buying-candies-with-discount Easy .java Array

---
+++ Java/Easy/2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount-05-31-2026-13-05-40.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minimumCost(int[] cost) {
+        Arrays.sort(cost);
+        int n = cost.length;
+        int total = 0;
+
+        int count = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            count++;
+            if (count == 3) {
+                count = 0;
+                continue;
+            }
+            total += cost[i];
+        }
+        return total;
+    }
+}
