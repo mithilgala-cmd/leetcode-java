069b5dd9969c657c37e48b6bc916d352e1ab4638
Mithil Gala <mithil.gala@gmail.com>
2026-06-08 16:26:38 +0530

    3 ms (81.41%) 43.3 MB (80.68%) 06-08-2026 0682-baseball-game Easy .java Array

---
+++ Java/Easy/0682-baseball-game/0682-baseball-game-06-08-2026-16-26-36.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (String op : operations) {
+            if (op.equals("C")) stack.pop();
+            else if (op.equals("D")) stack.push(stack.peek() * 2);
+            else if (op.equals("+")) {
+                int a = stack.pop(), b = stack.peek();
+                stack.push(a);
+                stack.push(a + b);
+            } else stack.push(Integer.parseInt(op));
+        }
+        int sum = 0;
+        for (int score : stack) sum += score;
+        return sum;
+    }
+}
