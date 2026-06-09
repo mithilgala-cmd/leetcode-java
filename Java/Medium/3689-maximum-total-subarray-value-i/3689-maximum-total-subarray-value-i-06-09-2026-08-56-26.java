7a09155dec2d5488ee0c8d4788dc96da0bd7c3b5
Mithil Gala <mithil.gala@gmail.com>
2026-06-09 08:56:26 +0530

    1 ms (100%) 62 MB (78.42%) 06-09-2026 3689-maximum-total-subarray-value-i Medium .java Array

---
+++ Java/Medium/3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i-06-09-2026-08-56-26.java
@@ -0,0 +1,10 @@
+class Solution {
+    public long maxTotalValue(int[] nums, int k) {
+        int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
+        for (int num : nums) {
+            max = Math.max(max, num);
+            min = Math.min(min, num);
+        }
+        return (long)(max - min) * k;
+    }
+}
