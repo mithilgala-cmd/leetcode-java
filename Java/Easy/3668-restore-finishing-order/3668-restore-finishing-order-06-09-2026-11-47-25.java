58dae4fda06d519e56b9ba614c3b88eddb30a334
Mithil Gala <mithil.gala@gmail.com>
2026-06-09 11:47:27 +0530

    3 ms (12.34%) 46.6 MB (80.06%) 06-09-2026 3668-restore-finishing-order Easy .java Array

---
+++ Java/Easy/3668-restore-finishing-order/3668-restore-finishing-order-06-09-2026-11-47-25.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] recoverOrder(int[] order, int[] friends) {
+        Set<Integer> set = new HashSet<>();
+        for (int f : friends) set.add(f);
+
+        List<Integer> result = new ArrayList<>();
+        for (int id : order) {
+            if (set.contains(id)) result.add(id);
+        }
+
+        int[] ans = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) ans[i] = result.get(i);
+        return ans;
+    }
+}
