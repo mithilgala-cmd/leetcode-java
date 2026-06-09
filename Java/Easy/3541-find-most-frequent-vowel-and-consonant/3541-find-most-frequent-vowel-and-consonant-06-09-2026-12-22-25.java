dba0d384edf674dee466a3e42ca5e56e6106834a
Mithil Gala <mithil.gala@gmail.com>
2026-06-09 12:22:27 +0530

    1 ms (100%) 43.7 MB (72.62%) 06-09-2026 3541-find-most-frequent-vowel-and-consonant Easy .java Hash Table

---
+++ Java/Easy/3541-find-most-frequent-vowel-and-consonant/3541-find-most-frequent-vowel-and-consonant-06-09-2026-12-22-25.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxFreqSum(String s) {
+        int[] freq = new int[26];
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+        
+        int maxVowel = 0, maxConsonant = 0;
+        for (int i = 0; i < 26; i++) {
+            if ("aeiou".indexOf((char)(i + 'a')) != -1) {
+                maxVowel = Math.max(maxVowel, freq[i]);
+            } else {
+                maxConsonant = Math.max(maxConsonant, freq[i]);
+            }
+        }        
+        return maxVowel + maxConsonant;
+    }
+}
