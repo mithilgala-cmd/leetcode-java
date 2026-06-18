20aefa8e6e14e2af359d64c406910fc00b4819d3
Mithil Gala <mithil.gala@gmail.com>
2026-06-18 16:31:57 +0530

    0 ms (100%) 44.4 MB (48.13%) 06-18-2026 2185-counting-words-with-a-given-prefix Easy .java Array

---
+++ Java/Easy/2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix-06-18-2026-16-31-55.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int prefixCount(String[] words, String pref) {
+        int count = 0;
+        for (String w : words) {
+            if (w.startsWith(pref)) count++;
+        }
+        return count;
+    }
+}
