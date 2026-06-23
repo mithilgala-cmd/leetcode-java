cf5de8638d7df336b7e48ef03a5a0dbc43dac1e2
Mithil Gala <mithil.gala@gmail.com>
2026-06-23 22:24:35 +0530

    0 ms (100%) 42 MB (80.43%) 06-23-2026 3099-harshad-number Easy .java Math

---
+++ Java/Easy/3099-harshad-number/3099-harshad-number-06-23-2026-22-24-34.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int sumOfTheDigitsOfHarshadNumber(int x) {
+        int sum = 0, temp = x;
+        while (temp > 0) {
+            sum += temp % 10;
+            temp /= 10;
+        }
+        return x % sum == 0 ? sum : -1;
+    }
+}
