f78067ff2a323ea56b24b2e25ccf68cf45829326
Mithil Gala <mithil.gala@gmail.com>
2026-07-01 21:01:59 +0530

    2 ms (70.57%) 47.1 MB (72.58%) 07-01-2026 0152-maximum-product-subarray Medium .java Array

---
+++ Java/Medium/0152-maximum-product-subarray/0152-maximum-product-subarray-07-01-2026-21-01-59.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int max = nums[0], min = nums[0], res = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            int cur = nums[i];
+            if (cur < 0) {
+                int temp = max;
+                max = min;
+                min = temp;
+            }
+            max = Math.max(cur, max * cur);
+            min = Math.min(cur, min * cur);
+            res = Math.max(res, max);
+        }
+        return res;
+    }
+}
