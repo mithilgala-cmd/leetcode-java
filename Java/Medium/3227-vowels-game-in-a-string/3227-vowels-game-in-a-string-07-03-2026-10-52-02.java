ddbc7d8a4287c0d0ba52a41c05b34fdd3c83f00e
Mithil Gala <mithil.gala@gmail.com>
2026-07-03 10:52:02 +0530

    16 ms (10.47%) 47.7 MB (65.84%) 07-03-2026 3227-vowels-game-in-a-string Medium .java Math

---
+++ Java/Medium/3227-vowels-game-in-a-string/3227-vowels-game-in-a-string-07-03-2026-10-52-02.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean doesAliceWin(String s) {
+        for (char c : s.toCharArray())
+            if ("aeiou".indexOf(c) != -1) return true;
+        return false;
+    }
+}
