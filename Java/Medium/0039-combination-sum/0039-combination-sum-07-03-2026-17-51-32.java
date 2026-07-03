cdcff10e42693921b4110e470b113ff60c5019c1
Mithil Gala <mithil.gala@gmail.com>
2026-07-03 17:51:31 +0530

    2 ms (96.13%) 45.7 MB (50.77%) 07-03-2026 0039-combination-sum Medium .java Array

---
+++ Java/Medium/0039-combination-sum/0039-combination-sum-07-03-2026-17-51-32.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(candidates, target, 0, new ArrayList<>(), res);
+        return res;
+    }
+
+    private void backtrack(int[] candidates, int target, int start, List<Integer> path, List<List<Integer>> res) {
+        if (target == 0) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = start; i < candidates.length; i++) {
+            if (candidates[i] > target) continue;
+            path.add(candidates[i]);
+            backtrack(candidates, target - candidates[i], i, path, res);
+            path.remove(path.size() - 1);
+        }
+    }
+}
