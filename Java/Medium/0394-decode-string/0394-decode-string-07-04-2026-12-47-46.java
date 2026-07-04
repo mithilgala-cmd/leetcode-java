2955cd1d8cf98facbcd0a5b3c570b148dfe76b0a
Mithil Gala <mithil.gala@gmail.com>
2026-07-04 12:47:46 +0530

    1 ms (86.08%) 42.8 MB (72.31%) 07-04-2026 0394-decode-string Medium .java String

---
+++ Java/Medium/0394-decode-string/0394-decode-string-07-04-2026-12-47-46.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String decodeString(String s) {
+        Stack<Integer> numStack = new Stack<>();
+        Stack<StringBuilder> strStack = new Stack<>();
+        StringBuilder curr = new StringBuilder();
+        int num = 0;
+
+        for (char c : s.toCharArray()) {
+            if (Character.isDigit(c)) {
+                num = num * 10 + (c - '0');
+            } else if (c == '[') {
+                numStack.push(num);
+                strStack.push(curr);
+                curr = new StringBuilder();
+                num = 0;
+            } else if (c == ']') {
+                int repeat = numStack.pop();
+                StringBuilder prev = strStack.pop();
+                for (int i = 0; i < repeat; i++) prev.append(curr);
+                curr = prev;
+            } else {
+                curr.append(c);
+            }
+        }
+        return curr.toString();
+    }
+}
