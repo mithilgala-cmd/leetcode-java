d334fdcfce583a1bb66c2dc21b73df3f35f6f62d
Mithil Gala <mithil.gala@gmail.com>
2026-07-12 00:10:09 +0530

    17 ms (96.13%) 85.1 MB (23.22%) 07-12-2026 1352-product-of-the-last-k-numbers Medium .java Array

---
+++ Java/Medium/1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers-07-12-2026-00-10-08.java
@@ -0,0 +1,31 @@
+class ProductOfNumbers {
+    List<Integer> prefix;
+
+    public ProductOfNumbers() {
+        prefix = new ArrayList<>();
+        prefix.add(1);
+    }
+
+    public void add(int num) {
+        if (num == 0) {
+            prefix.clear();
+            prefix.add(1);
+        } else {
+            prefix.add(prefix.get(prefix.size() - 1) * num);
+        }
+    }
+
+    public int getProduct(int k) {
+        if (k >= prefix.size()) return 0;
+
+        int n = prefix.size();
+        return prefix.get(n - 1) / prefix.get(n - k - 1);
+    }
+}
+
+/**
+ * Your ProductOfNumbers object will be instantiated and called as such:
+ * ProductOfNumbers obj = new ProductOfNumbers();
+ * obj.add(num);
+ * int param_2 = obj.getProduct(k);
+ */
