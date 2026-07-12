d739ea46924c74cb7c6793092a3b0f352e3efd6d
Mithil Gala <mithil.gala@gmail.com>
2026-07-12 11:38:33 +0530

    33 ms (18.75%) 50.7 MB (21.68%) 07-12-2026 0697-degree-of-an-array Easy .java Array

---
+++ Java/Easy/0697-degree-of-an-array/0697-degree-of-an-array-07-12-2026-11-38-34.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findShortestSubArray(int[] nums) {
+        Map<Integer, Integer> count = new HashMap<>();
+        Map<Integer, Integer> first = new HashMap<>();
+
+        int degree = 0;
+        int ans = nums.length;
+
+        for (int i = 0; i < nums.length; i++) {
+            first.putIfAbsent(nums[i], i);
+            count.put(nums[i], count.getOrDefault(nums[i], 0) + 1);
+
+            if (count.get(nums[i]) > degree) {
+                degree = count.get(nums[i]);
+                ans = i - first.get(nums[i]) + 1;
+            } else if (count.get(nums[i]) == degree) {
+                ans = Math.min(ans, i - first.get(nums[i]) + 1);
+            }
+        }
+        return ans;
+    }
+}
