ce2224277d7986f5d44e70427749f29305519e9e
Mithil Gala <mithil.gala@gmail.com>
2026-07-21 18:19:59 +0530

    1 ms (100%) 46.5 MB (25.37%) 07-21-2026 2706-buy-two-chocolates Easy .java Array

---
+++ Java/Easy/2706-buy-two-chocolates/2706-buy-two-chocolates-07-21-2026-18-19-59.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int buyChoco(int[] prices, int money) {
+        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+        for (int p : prices) {
+            if (p < min1) {
+                min2 = min1;
+                min1 = p;
+            } else if (p < min2) {
+                min2 = p;
+            }
+        }
+        int cost = min1 + min2;
+        return cost <= money ? money - cost : money;
+    }
+}
