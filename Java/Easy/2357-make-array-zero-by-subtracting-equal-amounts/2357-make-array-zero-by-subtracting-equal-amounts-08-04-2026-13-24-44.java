7491f559757f0d95d5c30bb10020d7c116aeccb5
Mithil Gala <mithil.gala@gmail.com>
2026-08-04 13:24:44 +0530

    1 ms (93.15%) 42.9 MB (84.22%) 08-04-2026 2357-make-array-zero-by-subtracting-equal-amounts Easy .java Array

---
+++ Java/Easy/2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts-08-04-2026-13-24-44.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int minimumOperations(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int a: nums)
+            if (a > 0) set.add(a);
+        return set.size();
+    }
+}
