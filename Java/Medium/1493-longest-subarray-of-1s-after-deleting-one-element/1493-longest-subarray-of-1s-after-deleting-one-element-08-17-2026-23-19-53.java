9109de711e04c928bcb297a9285d379bfcf8eabb
Mithil Gala <mithil.gala@gmail.com>
2026-08-17 23:19:54 +0530

    3 ms (87.33%) 65.4 MB (67.87%) 08-17-2026 1493-longest-subarray-of-1s-after-deleting-one-element Medium .java Array

---
+++ Java/Medium/1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element-08-17-2026-23-19-53.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int longestSubarray(int[] nums) {
+        int left = 0, zeroCount = 0, res = 0;
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] == 0) zeroCount++;
+            while (zeroCount > 1) {
+                if (nums[left++] == 0) zeroCount--;
+            }
+            res = Math.max(res, right - left);
+        }
+        return res;
+    }
+}
